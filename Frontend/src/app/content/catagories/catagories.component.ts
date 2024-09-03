d128aeabff070d99f5d7876a1ef02832a58e1f42
Henru Olivier <henrupetrusolivier@gmail.com>
2024-09-03 20:34:37 +0200

    catagories styling

--- Frontend/src/app/content/catagories/catagories.component.ts
+++ Frontend/src/app/content/catagories/catagories.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ToggleInViewDirective } from '../../shared/directives/toggleInViewClass';
+import { TypingEffectDirective } from '../../shared/directives/typingEffect';
 
 @Component({
   selector: 'app-catagories',
   standalone: true,
-  imports: [],
+  imports: [TypingEffectDirective, ToggleInViewDirective],
   templateUrl: './catagories.component.html',
   styleUrl: './catagories.component.scss'
 })
