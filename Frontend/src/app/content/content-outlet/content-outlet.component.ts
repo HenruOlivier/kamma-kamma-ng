809458855a33dfe7138ccce498c0870608434200
Henru Olivier <henrupetrusolivier@gmail.com>
2024-09-03 20:18:19 +0200

    layout update

--- Frontend/src/app/content/content-outlet/content-outlet.component.ts
+++ Frontend/src/app/content/content-outlet/content-outlet.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { IntroPageComponent } from '../intro-page/intro-page.component';
 import { IntroductionComponent } from '../introduction/introduction.component';
 import { ServicesComponent } from '../services/services.component';
+import { CatagoriesComponent } from '../catagories/catagories.component';
 
 @Component({
   selector: 'app-content-outlet',
   standalone: true,
-  imports: [IntroPageComponent, IntroductionComponent, ServicesComponent],
+  imports: [IntroPageComponent, IntroductionComponent, ServicesComponent, CatagoriesComponent],
   templateUrl: './content-outlet.component.html',
   styleUrl: './content-outlet.component.scss'
 })
