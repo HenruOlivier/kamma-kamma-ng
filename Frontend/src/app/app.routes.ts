782a9322616410d18fea9bd122a02d1c87f885d1
Henru Olivier <henrupetrusolivier@gmail.com>
2024-09-05 19:13:34 +0200

    layout update

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ContentOutletComponent } from './layout/body-content/content-outlet/content-outlet.component';
+import { SearchAllComponent } from './content/search-all/search-all.component';
 
 export const routes: Routes = [
-    { path: '', component: ContentOutletComponent }
+    { path: '', component: ContentOutletComponent },
+    { path: 'search', component: SearchAllComponent },
 ];
--- Frontend/src/app/content/search-bar/search-bar.component.ts
+++ Frontend/src/app/content/search-bar/search-bar.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
+
+  searchQuery: string = '';  // Hold the search query
+
+  constructor(private router: Router) {}
+
+  // Method to handle the keydown event
+  onSearchKeyDown(event: KeyboardEvent) {
+    console.log('keyyy ', event)
+    if (event.key === 'Enter') {
+      // Navigate to the /search route, optionally with query parameters
+      this.router.navigate(['/search'], { queryParams: { q: this.searchQuery } });
+    }
+  }
 
 }
