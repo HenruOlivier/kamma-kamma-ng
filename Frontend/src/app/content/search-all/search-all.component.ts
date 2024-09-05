067317da2e9a26db9754b17ba7a9ded89d7d8fed
Henru Olivier <henrupetrusolivier@gmail.com>
2024-09-05 19:29:24 +0200

    search page styling update

--- Frontend/src/app/content/search-all/search-all.component.ts
+++ Frontend/src/app/content/search-all/search-all.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-all',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-all.component.html',
   styleUrl: './search-all.component.scss'
 })
 export class SearchAllComponent {
+
+  options: string[] = ['awe', 'jaaaaa'];
 
 }
