2a7dfe5971dad09a79d93f77a443b7805eb18bd9
Henru Olivier <henrupetrusolivier@gmail.com>
2024-09-05 18:39:38 +0200

    search bar added

--- Frontend/src/app/layout/header/header.component.ts
+++ Frontend/src/app/layout/header/header.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { UitoolsService } from '../../shared/services/uitools.service';
+import { SearchBarComponent } from '../../content/search-bar/search-bar.component';
+import { SearchAllComponent } from "../../content/search-all/search-all.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchBarComponent, SearchAllComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
