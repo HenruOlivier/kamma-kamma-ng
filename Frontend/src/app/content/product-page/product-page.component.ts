832b36db4b54751672ead6abb77e3d4a59a5801b
Henru Olivier <henrupetrusolivier@gmail.com>
2024-09-11 21:13:37 +0200

    product page listening for changes in url and fetching current product accordingly

--- Frontend/src/app/content/product-page/product-page.component.ts
+++ Frontend/src/app/content/product-page/product-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from '../../shared/services/products.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-product-page',
@@ -8,6 +10,24 @@ import { Router } from '@angular/router';
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss'
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit{
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductsService
+  ) {}
+
+  ngOnInit(): void {
+     // Listen to changes in the 'q' query parameter and fetch results
+     this.route.queryParams
+     .pipe(
+       // Listen to changes in the `q` query parameter
+       switchMap(params => {
+         const searchQuery = params['p'] || ''; // Get the 'q' parameter or default to an empty string
+         return this.productService.fetchCurrentProduct(searchQuery); // Fetch data with the search query
+       })
+     )
+     .subscribe(); // Subscribe to trigger the fetch
+  }
 
 }
--- Frontend/src/app/shared/services/products.service.ts
+++ Frontend/src/app/shared/services/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, catchError, finalize, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +8,52 @@ import { Observable } from 'rxjs';
 export class ProductsService {
 
   private baseUrl: string = 'http://localhost:5000/api/products';  // Adjust this URL if necessary
+  
+  private currentProductLoadingSubject = new BehaviorSubject<boolean>(false);
+  public get currentProductLoading$(): Observable<boolean> {
+    return this.currentProductLoadingSubject.asObservable();
+  }
+
+  private currentProductSubject = new BehaviorSubject<any[]>([]);
+  public get currentProduct$(): Observable<any[]> {
+    return this.currentProductSubject.asObservable();
+  }
 
   constructor(private http: HttpClient) {}
 
-  
+  // Fetch products based on the search text
+  fetchCurrentProduct(searchText: string): Observable<any> {
+    console.log('fetchFromsearch called')
+    this.currentProductLoadingSubject.next(true);
+
+    return this.http.get<any>(this.baseUrl)
+    .pipe(
+      tap((res) => {
+        // If successful, update the local state with the fetched products
+        this.currentProductSubject.next(res);
+      }),
+      catchError(error => {
+        // Handle the error appropriately
+        console.error('Error while fetching products:', error);
+
+        let errorMessage = 'Error while fetching products';
+        if (error.error && error.error.message) {
+          errorMessage = error.error.message;
+        }
+
+        // Notify failure
+        return of(null);
+      }),
+      finalize(() => {
+        this.currentProductLoadingSubject.next(false);
+      })
+    );
+  }
+
+  // Fetch all products from backend
+  // getProducts(): Observable<any> {
+  //   return this.http.get<any>(this.baseUrl);
+  // }
 
   // Fetch all products from backend
   // getProducts(): Observable<any> {
