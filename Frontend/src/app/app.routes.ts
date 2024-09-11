549826ff7f617a9ad5818886daec9d313f62ba7f
Henru Olivier <henrupetrusolivier@gmail.com>
2024-09-11 21:00:07 +0200

    product page comp and routing

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { ContentOutletComponent } from './layout/body-content/content-outlet/content-outlet.component';
 import { SearchAllComponent } from './content/search-all/search-all.component';
+import { ProductPageComponent } from './content/product-page/product-page.component';
 
 export const routes: Routes = [
     { path: '', component: ContentOutletComponent },
     { path: 'search', component: SearchAllComponent },
-    { path: 'search/:_searchText', component: SearchAllComponent },
+    { path: 'productPage', component: ProductPageComponent },
 ];
