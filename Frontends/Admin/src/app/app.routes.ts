1cf97e6b9b1bdb10c60eeb1e02b0eb546f598c7e
Henru Olivier <henrupetrusolivier@gmail.com>
2024-10-08 21:26:15 +0200

    added styling files to admin page

--- Frontends/Admin/src/app/app.routes.ts
+++ Frontends/Admin/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { ContentOutletComponent } from './layout/body-content/content-outlet/content-outlet.component';
 import { ProductGridComponent } from './functions/products/product-grid/product-grid.component';
+import { ProductFormComponent } from './functions/products/product-form/product-form.component';
 
 export const routes: Routes = [
     { path: '', component: ContentOutletComponent },
     { path: 'products', component:  ProductGridComponent},
-    { path: '', component: ContentOutletComponent },
+    { path: 'products/form', component: ProductFormComponent },
     { path: '', component: ContentOutletComponent },
     { path: '', component: ContentOutletComponent },
 ];
