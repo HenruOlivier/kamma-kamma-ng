e0f2123e7448fe115cca7766067de1e8cd4c4a0c
Henru Olivier <henrupetrusolivier@gmail.com>
2024-10-22 21:45:13 +0200

    routes for catagories

--- Frontends/Admin/src/app/app.routes.ts
+++ Frontends/Admin/src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 import { ContentOutletComponent } from './layout/body-content/content-outlet/content-outlet.component';
 import { ProductGridComponent } from './functions/products/product-grid/product-grid.component';
 import { ProductFormComponent } from './functions/products/product-form/product-form.component';
+import { CatagoryGridComponent } from './functions/catagories/catagory-grid/catagory-grid.component';
+import { CatagoryFormComponent } from './functions/catagories/catagory-form/catagory-form.component';
 
 export const routes: Routes = [
     { path: '', component: ContentOutletComponent },
     { path: 'products', component:  ProductGridComponent},
     { path: 'products/form', component: ProductFormComponent },
     { path: 'products/form/:_id', component: ProductFormComponent },
-    { path: '', component: ContentOutletComponent },
+    { path: 'catagories', component:  CatagoryGridComponent},
+    { path: 'catagories/form', component: CatagoryFormComponent },
+    { path: 'products/form/:_id', component: CatagoryFormComponent },
     { path: '', component: ContentOutletComponent },
 ];
