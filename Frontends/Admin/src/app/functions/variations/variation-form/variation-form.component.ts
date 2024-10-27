6fe33101d5e24b61b1fd8374f6befc3f262553b1
Henru Olivier <henrupetrusolivier@gmail.com>
2024-10-27 22:38:28 +0200

    variation for component update to be used in product form

--- Frontends/Admin/src/app/functions/variations/variation-form/variation-form.component.ts
+++ Frontends/Admin/src/app/functions/variations/variation-form/variation-form.component.ts
@@ -1,16 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { SSComponentsModule } from '../../../shared/components/ss-lib-components/ss-components.module';
 import { SSDirectivesModule } from '../../../shared/ss-directives/ss-directives.module';
 import { ActionButtonStates } from '../../../shared/ss-directives/action-button-states';
 import { Subject } from 'rxjs';
-import { ProductVariation } from '../../../shared/models/productVariation.model';
 import { FormFieldDefinition, FormFieldTypes } from '../../../shared/components/ss-lib-components/ss-form-builder2/form-fields2.model';
-import { ActivatedRoute } from '@angular/router';
-import { VariationsService } from '../../../shared/services/variations/variations.service';
+// import { ActivatedRoute } from '@angular/router';
 import { SSFormController } from '../../../shared/components/ss-lib-components/ss-form-builder2/ss-form-controller.service';
-import { catchError, finalize, tap } from 'rxjs/operators';
 import { ProductsService } from '../../../shared/services/products/products.service';
+import { VariationOption } from '../../../shared/models/variationOption.model';
 
 @Component({
   selector: 'app-variation-form',
@@ -21,24 +19,27 @@ import { ProductsService } from '../../../shared/services/products/products.serv
 })
 export class VariationFormComponent implements OnInit, OnDestroy {
 
+  @Output() saveVariation: EventEmitter<VariationOption> = new EventEmitter();
+
   destroy$ = new Subject<void>();
 
   _id: string | null = null;
-  currentVariationEditable: ProductVariation | null = null;
+  currentVariationEditable: VariationOption | null = null;
 
   formDefinition: FormFieldDefinition[] = [];
 
   btnState = ActionButtonStates.Idle;
   btnError = ActionButtonStates.Error;
   btnText = 'Save Variation';
+  
 
   constructor(
-    private activatedRoute: ActivatedRoute,
-    public variationsService: VariationsService,
+    // private activatedRoute: ActivatedRoute,
+    // public variationsService: VariationsService,
     private productsService: ProductsService,
     public formController: SSFormController
   ) {
-    // Define form fields according to the ProductVariation interface
+    // Define form fields according to the VariationOption interface
     this.formDefinition = [
       { type: FormFieldTypes.Text, name: 'optionName', label: 'Option Name', required: true },
       { type: FormFieldTypes.Text, name: 'optionValue', label: 'Option Value', required: true },
@@ -57,63 +58,64 @@ export class VariationFormComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this._id = this.activatedRoute.snapshot.params['_id'] || null;
+    // this._id = this.activatedRoute.snapshot.params['_id'] || null;
 
-    if (this._id) {
-      this.variationsService.fetchVariationById(this._id)
-        .pipe(
-          tap((response: ProductVariation | null) => {
-            if (response) {
-              this.currentVariationEditable = response;
-              this.formController.setFormValue(response);
-            }
-          }),
-          catchError((error: any) => {
-            console.error('Error fetching variation:', error);
-            return error;
-          })
-        )
-        .subscribe();
-    }
+    // if (this._id) {
+    //   this.variationsService.fetchVariationById(this._id)
+    //     .pipe(
+    //       tap((response: VariationOption | null) => {
+    //         if (response) {
+    //           this.currentVariationEditable = response;
+    //           this.formController.setFormValue(response);
+    //         }
+    //       }),
+    //       catchError((error: any) => {
+    //         console.error('Error fetching variation:', error);
+    //         return error;
+    //       })
+    //     )
+    //     .subscribe();
+    // }
   }
 
   onSave() {
-    const formData: ProductVariation = this.formController.getFormValue();
+    const formData: VariationOption = this.formController.getFormValue();
     this.btnState = ActionButtonStates.Loading;
 
-    if (this._id) {
-      this.variationsService.updateVariation(this._id, formData)
-        .pipe(
-          tap(() => {
-            this.btnState = ActionButtonStates.Success;
-          }),
-          catchError((error: any) => {
-            this.btnState = ActionButtonStates.Error;
-            console.error('Error updating variation:', error);
-            return error;
-          }),
-          finalize(() => {
-            this.btnState = ActionButtonStates.Idle;
-          })
-        )
-        .subscribe();
-    } else {
-      this.variationsService.addVariation(formData)
-        .pipe(
-          tap(() => {
-            this.btnState = ActionButtonStates.Success;
-          }),
-          catchError((error: any) => {
-            this.btnState = ActionButtonStates.Error;
-            console.error('Error creating variation:', error);
-            return error;
-          }),
-          finalize(() => {
-            this.btnState = ActionButtonStates.Idle;
-          })
-        )
-        .subscribe();
-    }
+    this.saveVariation.emit(formData);
+    // if (this._id) {
+    //   this.variationsService.updateVariation(this._id, formData)
+    //     .pipe(
+    //       tap(() => {
+    //         this.btnState = ActionButtonStates.Success;
+    //       }),
+    //       catchError((error: any) => {
+    //         this.btnState = ActionButtonStates.Error;
+    //         console.error('Error updating variation:', error);
+    //         return error;
+    //       }),
+    //       finalize(() => {
+    //         this.btnState = ActionButtonStates.Idle;
+    //       })
+    //     )
+    //     .subscribe();
+    // } else {
+    //   this.variationsService.addVariation(formData)
+    //     .pipe(
+    //       tap(() => {
+    //         this.btnState = ActionButtonStates.Success;
+    //       }),
+    //       catchError((error: any) => {
+    //         this.btnState = ActionButtonStates.Error;
+    //         console.error('Error creating variation:', error);
+    //         return error;
+    //       }),
+    //       finalize(() => {
+    //         this.btnState = ActionButtonStates.Idle;
+    //       })
+    //     )
+    //     .subscribe();
+    // }
   }
 
   ngOnDestroy() {
