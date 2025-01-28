40fc4c32baf83f538d559dfc816c989421124e8c
Henru Olivier <henrupetrusolivier@gmail.com>
2025-01-28 15:08:16 +0200

    referencing udpate

--- Frontends/Admin/src/app/functions/images/image-form/image-form.component.ts
+++ Frontends/Admin/src/app/functions/images/image-form/image-form.component.ts
@@ -10,11 +10,12 @@ import { ActivatedRoute } from '@angular/router';
 import { ImagesService } from '../../../shared/services/images/images.service';
 import { SSFormController } from '../../../shared/components/ss-lib-components/ss-form-builder2/ss-form-controller.service';
 import { catchError, finalize, tap } from 'rxjs/operators';
+import { SSFormBuilder2Component } from '../../../shared/components/ss-lib-components/ss-form-builder2/ss-form-builder2.component';
 
 @Component({
   selector: 'app-image-form',
   standalone: true,
-  imports: [CommonModule, SSComponentsModule, SSDirectivesModule],
+  imports: [CommonModule, SSComponentsModule, SSDirectivesModule, SSFormBuilder2Component],
   templateUrl: './image-form.component.html',
   styleUrls: ['./image-form.component.scss']
 })
