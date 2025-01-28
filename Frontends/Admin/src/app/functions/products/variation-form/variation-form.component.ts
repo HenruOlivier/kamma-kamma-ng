a543d0211db76d81277d344f9bb22e1b5c4753c9
Henru Olivier <henrupetrusolivier@gmail.com>
2025-01-28 15:09:01 +0200

    variation form updated and now successfully managing product variations

--- Frontends/Admin/src/app/functions/products/variation-form/variation-form.component.ts
+++ Frontends/Admin/src/app/functions/products/variation-form/variation-form.component.ts
@@ -1,27 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { SSComponentsModule } from '../../../shared/components/ss-lib-components/ss-components.module';
 import { SSDirectivesModule } from '../../../shared/ss-directives/ss-directives.module';
 import { ActionButtonStates } from '../../../shared/ss-directives/action-button-states';
-import { FormFieldDefinition, FormFieldTypes } from '../../../shared/components/ss-lib-components/ss-form-builder2/form-fields2.model';
+// import { FormFieldDefinition, FormFieldTypes } from '../../../shared/components/ss-lib-components/ss-form-builder2/form-fields2.model';
 import { ProductsService } from '../../../shared/services/products/products.service';
 import { SSFormController } from '../../../shared/components/ss-lib-components/ss-form-builder2/ss-form-controller.service';
 import { ProductVariation } from '../../../shared/models/productVariation.model';
+import { SSInputTextComponent } from '../../../shared/components/ss-lib-components/ss-input-text/ss-input-text.component';
+import { SSInputNumberComponent } from '../../../shared/components/ss-lib-components/ss-input-number/ss-input-number.component';
 
 @Component({
   selector: 'app-variation-form',
   standalone: true,
-  imports: [CommonModule, SSComponentsModule, SSDirectivesModule],
+  imports: [CommonModule, SSComponentsModule, SSDirectivesModule, SSInputTextComponent, SSInputNumberComponent],
   templateUrl: './variation-form.component.html',
   styleUrl: './variation-form.component.scss'
 })
 export class VariationFormComponent {
 
+  @Input() productVariations: ProductVariation[] = [];
+
   @Output() addVariation = new EventEmitter<ProductVariation[]>();
 
-  productVariations: ProductVariation[] = [];
-
-  formDefinition: FormFieldDefinition[] = [];
+  // formDefinition: FormFieldDefinition[] = [];
 
   btnState = ActionButtonStates.Idle;
   btnError = ActionButtonStates.Error;
@@ -36,16 +38,17 @@ export class VariationFormComponent {
 
   constructor(
     public productsService: ProductsService,
-    public formController: SSFormController,
+    // public formController: SSFormController,
   ) {
-    this.formDefinition = [
-      { type: FormFieldTypes.Text, name: 'name', label: 'Variation Name', required: true },
-      { type: FormFieldTypes.Number, name: 'stockQuantity', label: 'Stock Quantity', required: true },
-    ];
+    // this.formDefinition = [
+    //   { type: FormFieldTypes.Text, name: 'name', label: 'Variation Name', required: true },
+    //   { type: FormFieldTypes.Number, name: 'stockQuantity', label: 'Stock Quantity', required: true },
+    // ];
   }
 
   onSaveVariation() {
-    const variationData: ProductVariation = this.formController.getFormValue();
+    // const variationData: ProductVariation = this.formController.getFormValue();
+    const variationData: ProductVariation = {name: this.currentName, stockQuantity: this.currentStock, images: []};
     console.log('Variation data to save:', variationData);
     this.productVariations.push(variationData);
     this.addVariation.emit(this.productVariations);
