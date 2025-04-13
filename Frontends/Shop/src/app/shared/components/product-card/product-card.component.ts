fc70dae44ea8a1f49455df44f25a9be44fcb5b58
Henru Olivier <henrupetrusolivier@gmail.com>
2025-04-13 18:01:25 +0200

    product card using Product input

--- Frontends/Shop/src/app/shared/components/product-card/product-card.component.ts
+++ Frontends/Shop/src/app/shared/components/product-card/product-card.component.ts
@@ -1,15 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent {
-
   @Input() product: Product | null = null;
-
 }
